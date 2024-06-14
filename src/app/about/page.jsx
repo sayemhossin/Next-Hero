ca4cd84fe389c93248e451f7211f9e4403428d50
sayemhossin <sayemhossin22@gmail.com>
2024-06-15 00:13:11 +0600

    ok

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -10,21 +10,21 @@ export const metadata = {
 };
 
 
-const getTime = async () => {
-    const res = await fetch('http://localhost:3000/time', { next: { revalidate: 3 } })
-    // const res = await fetch('http://localhost:3000/time', {cache: 'no-store'})
-    const data = await res.json()
-    return data.currentTime
-}
+// const getTime = async () => {
+//     const res = await fetch('http://localhost:3000/time', { next: { revalidate: 3 } })
+//     // const res = await fetch('http://localhost:3000/time', {cache: 'no-store'})
+//     const data = await res.json()
+//     return data.currentTime
+// }
 
 
 
 const page = async () => {
-    const currentTime = getTime()
+    // const currentTime = getTime()
     return (
         <div className={`${headland.className} h-screen `}>
             <h6 className='text-3xl'>About Page</h6>
-            <h6 className='text-3xl text-red-400 my-12'>Time: {currentTime}</h6>
+            {/* <h6 className='text-3xl text-red-400 my-12'>Time: {currentTime}</h6> */}
         </div>
     );
 };
